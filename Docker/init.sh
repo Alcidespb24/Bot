320197b5a37c5950976e9a66e16d16012c389d3b
CM <cmedi067@gmail.com>
2022-09-18 21:36:56 -0400

    updating sleep time to 30 s

--- Docker/init.sh
+++ Docker/init.sh
@@ -6,6 +6,6 @@ sleep 5s
 
 DISPLAY=:2 python -m mt5linux -w wine64 /root/.wine/drive_c/users/root/AppData/Local/Programs/Python/Python310/python.exe > /dev/null & 
 
-sleep 15s
+sleep 30s
 
 DISPLAY=:2 python /app/main.py
