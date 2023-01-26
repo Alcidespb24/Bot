95d2a25e38f1a8a79e1132a52cecfacd743d9d6a
Alcidespb24 <alcidesperezb@gmail.com>
2023-01-26 00:35:09 -0500

    optimizing the code

--- src/dashboard.py
+++ src/dashboard.py
@@ -11,8 +11,15 @@ import plotly.io as pio
 pio.templates
 warnings.filterwarnings('ignore')
 warnings.simplefilter('ignore')
+
 df_eth = trades_eth(minutes=5)
 
+def get_all_trades():
+    global df_eth
+    df_eth = trades_eth(minutes=5)
+    df_eth.dropna()
+    df_eth = df_eth.round(2)
+    
 app = dash.Dash(__name__)
 
 server = app.server
@@ -115,6 +122,7 @@ def time_average_price_figure_callback(n):
 @app.callback(Output('df_live_update', 'data'),
               Input('live_update_interval', 'n_intervals'))
 def data_table_update(n):
+    get_all_trades()
     global df_eth
     df_5m_lvalues = df_eth.dropna().tail(5)
     return df_5m_lvalues.to_dict('records')
